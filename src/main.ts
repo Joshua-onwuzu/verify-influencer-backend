9dc15b3141d744d4914af7faa6fdc5c1a7419a79
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-01-31 05:08:27 +0100

    enabled cors for fe domain

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://loquacious-bonbon-7f72d2.netlify.app',
+  });
 
   const PORT = process.env.PORT || 8080;
 
