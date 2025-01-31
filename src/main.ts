ccccef446de39c986da43240abaa6aa6e1fc11de
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-01-31 05:15:47 +0100

    removed cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://loquacious-bonbon-7f72d2.netlify.app',
-  });
-
   const PORT = process.env.PORT || 8080;
 
   await app.listen(PORT, () => {
